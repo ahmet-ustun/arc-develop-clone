c9b125bf2afb0f72c672092320994ad837792f65
Ahmet Ustun <austu@softserveinc.com>
2022-08-22 08:59:42 +0300

    creating the header using AppBar and Toolbar from Material UI

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,11 @@
 import React from "react";
 
+import Header from "./ui/Header.js";
+
 function App() {
   return (
     <div className="App">
+      <Header />
       Hello world!
     </div>
   );
