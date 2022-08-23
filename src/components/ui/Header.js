6046408034479e680d6a731ac4b3609b83e6d0fe
Ahmet Ustun <austu@softserveinc.com>
2022-08-23 09:41:33 +0300

    adding tabs to the Header component and styling them via the Theme component

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -2,6 +2,8 @@ import React from "react";
 
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
+import Tabs from "@material-ui/core/Tabs";
+import Tab from "@material-ui/core/Tab";
 
 import { useScrollTrigger } from "@material-ui/core";
 import { makeStyles } from "@material-ui/styles";
@@ -29,6 +31,14 @@ const useStyles = makeStyles((theme) => ({
   logo: {
     height: "7em",
   },
+  tabContainer: {
+    marginLeft: "auto",
+  },
+  tab: {
+    ...theme.typography.tab,
+    minWidth: 10,
+    marginLeft: "25px",
+  },
 }));
 
 function Header() {
@@ -40,6 +50,13 @@ function Header() {
         <AppBar>
           <Toolbar disableGutters>
             <img className={classes.logo} src={logo} alt="Company Logo" />
+            <Tabs className={classes.tabContainer}>
+              <Tab className={classes.tab} label="Home" />
+              <Tab className={classes.tab} label="Services" />
+              <Tab className={classes.tab} label="The Revolution" />
+              <Tab className={classes.tab} label="About Us" />
+              <Tab className={classes.tab} label="Contact Us" />
+            </Tabs>
           </Toolbar>
         </AppBar>
       </ElevationScroll>
