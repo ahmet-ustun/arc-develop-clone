0ce9d1e00b33cedfe159ecae1224a6eb2dab1cd7
Ahmet Ustun <austu@softserveinc.com>
2022-08-23 08:26:07 +0300

    adding the logo in the Header component and styling it

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -2,9 +2,11 @@ import React from "react";
 
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
-import Typography from "@material-ui/core/Typography";
 
 import { useScrollTrigger } from "@material-ui/core";
+import { makeStyles } from "@material-ui/styles";
+
+import logo from "../../assets/logo.svg";
 
 function ElevationScroll(props) {
   const { children } = props;
@@ -19,15 +21,30 @@ function ElevationScroll(props) {
   });
 }
 
+const useStyles = makeStyles((theme) => ({
+  toolbarMargin: {
+    ...theme.mixins.toolbar,
+    marginBottom: "3em",
+  },
+  logo: {
+    height: "7em",
+  },
+}));
+
 function Header() {
+  const classes = useStyles();
+
   return (
-    <ElevationScroll>
-      <AppBar>
-        <Toolbar>
-          <Typography variant="h3">Arc Development</Typography>
-        </Toolbar>
-      </AppBar>
-    </ElevationScroll>
+    <React.Fragment>
+      <ElevationScroll>
+        <AppBar>
+          <Toolbar disableGutters>
+            <img className={classes.logo} src={logo} alt="Company Logo" />
+          </Toolbar>
+        </AppBar>
+      </ElevationScroll>
+      <div className={classes.toolbarMargin} />
+    </React.Fragment>
   );
 }
 
