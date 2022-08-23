8fb95dd16af8e0ae36b2b96a6b7074ac33b812d2
Ahmet Ustun <austu@softserveinc.com>
2022-08-23 10:32:29 +0300

    adding the Free Estimation button to the Header component and styling it via the Theme component

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Tabs from "@material-ui/core/Tabs";
 import Tab from "@material-ui/core/Tab";
+import Button from "@material-ui/core/Button";
 
 import { useScrollTrigger } from "@material-ui/core";
 import { makeStyles } from "@material-ui/styles";
@@ -39,10 +40,22 @@ const useStyles = makeStyles((theme) => ({
     minWidth: 10,
     marginLeft: "25px",
   },
+  button: {
+    ...theme.typography.estimate,
+    borderRadius: "50px",
+    marginLeft: "50px",
+    marginRight: "25px",
+    height: "45px",
+  },
 }));
 
 function Header() {
   const classes = useStyles();
+  const [value, setValue] = useState(0);
+
+  const handleChange = (event, value) => {
+    setValue(value);
+  };
 
   return (
     <React.Fragment>
@@ -50,13 +63,24 @@ function Header() {
         <AppBar>
           <Toolbar disableGutters>
             <img className={classes.logo} src={logo} alt="Company Logo" />
-            <Tabs className={classes.tabContainer}>
+            <Tabs
+              className={classes.tabContainer}
+              value={value}
+              onChange={handleChange}
+            >
               <Tab className={classes.tab} label="Home" />
               <Tab className={classes.tab} label="Services" />
               <Tab className={classes.tab} label="The Revolution" />
               <Tab className={classes.tab} label="About Us" />
               <Tab className={classes.tab} label="Contact Us" />
             </Tabs>
+            <Button
+              className={classes.button}
+              variant="contained"
+              color="secondary"
+            >
+              Free Estimate
+            </Button>
           </Toolbar>
         </AppBar>
       </ElevationScroll>
