73db9ca97768302fd1b0bc0ec8a85d8a959ca62a
Ahmet Ustun <austu@softserveinc.com>
2022-08-29 10:38:25 +0300

    refactoring the header component by iterating the menu items via array

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -72,39 +72,93 @@ const useStyles = makeStyles((theme) => ({
 
 function Header() {
   const classes = useStyles();
-  const [value, setValue] = useState(0);
+  const [tabValue, setTabValue] = useState(0);
   const [anchorEl, setAnchorEl] = useState(null);
-  const [isOpen, setIsOpen] = useState(false);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [itemIndex, setItemIndex] = useState(0);
 
   const handleChange = (event, value) => {
-    setValue(value);
+    setTabValue(value);
   };
 
   const handleClick = (event) => {
     setAnchorEl(event.target);
-    setIsOpen(true);
+    setIsMenuOpen(true);
   };
 
   const handleClose = (event) => {
     setAnchorEl(null);
-    setIsOpen(false);
+    setIsMenuOpen(false);
   };
 
+  const handleMenuItem = (event, index) => {
+    setAnchorEl(null);
+    setIsMenuOpen(false);
+    setItemIndex(index);
+  };
+
+  const menuItems = [
+    { name: "Services", path: "/services" },
+    { name: "Software Development", path: "/services/software" },
+    { name: "Mobile App Development", path: "/services/mobile" },
+    { name: "Website Development", path: "/services/website" },
+  ];
+
   useEffect(() => {
-    if (window.location.pathname === "/" && value !== 0) {
-      setValue(0);
-    } else if (window.location.pathname === "/services" && value !== 1) {
-      setValue(1);
-    } else if (window.location.pathname === "/revolution" && value !== 2) {
-      setValue(2);
-    } else if (window.location.pathname === "/about" && value !== 3) {
-      setValue(3);
-    } else if (window.location.pathname === "/contact" && value !== 4) {
-      setValue(4);
-    } else if (window.location.pathname === "/estimate" && value !== 5) {
-      setValue(5);
+    switch (window.location.pathname) {
+      case "/":
+        if (tabValue !== 0) {
+          setTabValue(0);
+        }
+        break;
+      case "/services":
+        if (tabValue !== 1) {
+          setTabValue(1);
+          setItemIndex(0);
+        }
+        break;
+      case "/services/software":
+        if (tabValue !== 1) {
+          setTabValue(1);
+          setItemIndex(1);
+        }
+        break;
+      case "/services/mobile":
+        if (tabValue !== 1) {
+          setTabValue(1);
+          setItemIndex(2);
+        }
+        break;
+      case "/services/website":
+        if (tabValue !== 1) {
+          setTabValue(1);
+          setItemIndex(3);
+        }
+        break;
+      case "/revolution":
+        if (tabValue !== 2) {
+          setTabValue(2);
+        }
+        break;
+      case "/about":
+        if (tabValue !== 3) {
+          setTabValue(3);
+        }
+        break;
+      case "/contact":
+        if (tabValue !== 4) {
+          setTabValue(4);
+        }
+        break;
+      case "/estimate":
+        if (tabValue !== 5) {
+          setTabValue(5);
+        }
+        break;
+      default:
+        break;
     }
-  }, [value]);
+  }, [tabValue]);
 
   return (
     <React.Fragment>
@@ -116,13 +170,13 @@ function Header() {
               component={Link}
               to="/"
               disableRipple
-              onClick={() => setValue(0)}
+              onClick={() => setTabValue(0)}
             >
               <img className={classes.logo} src={logo} alt="Company Logo" />
             </Button>
             <Tabs
               className={classes.tabContainer}
-              value={value}
+              value={tabValue}
               onChange={handleChange}
             >
               <Tab
@@ -175,54 +229,26 @@ function Header() {
               id="simple-menu"
               classes={{ paper: classes.menu }}
               anchorEl={anchorEl}
-              open={isOpen}
-              elevation="0"
+              open={isMenuOpen}
+              elevation={0}
               MenuListProps={{ onMouseLeave: handleClose }}
             >
-              <MenuItem
-                classes={{ root: classes.menuItem }}
-                component={Link}
-                to="/services"
-                onClick={() => {
-                  handleClose();
-                  setValue(1);
-                }}
-              >
-                Services
-              </MenuItem>
-              <MenuItem
-                classes={{ root: classes.menuItem }}
-                component={Link}
-                to="/services/software"
-                onClick={() => {
-                  handleClose();
-                  setValue(1);
-                }}
-              >
-                Software Development
-              </MenuItem>
-              <MenuItem
-                classes={{ root: classes.menuItem }}
-                component={Link}
-                to="/services/mobile"
-                onClick={() => {
-                  handleClose();
-                  setValue(1);
-                }}
-              >
-                Mobile App Development
-              </MenuItem>
-              <MenuItem
-                classes={{ root: classes.menuItem }}
-                component={Link}
-                to="/services/website"
-                onClick={() => {
-                  handleClose();
-                  setValue(1);
-                }}
-              >
-                Website Development
-              </MenuItem>
+              {menuItems.map((menuItem, index) => (
+                <MenuItem
+                  classes={{ root: classes.menuItem }}
+                  key={menuItem.name}
+                  component={Link}
+                  to={menuItem.path}
+                  selected={index === itemIndex && tabValue === 1}
+                  onClick={(event) => {
+                    handleMenuItem(event, index);
+                    setTabValue(1);
+                    handleClose();
+                  }}
+                >
+                  {menuItem.name}
+                </MenuItem>
+              ))}
             </Menu>
           </Toolbar>
         </AppBar>
