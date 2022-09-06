38bbdb7a71380f1f7a280d7463f3aa341fdef53b
Ahmet Ustun <austu@softserveinc.com>
2022-09-06 18:26:12 +0300

    Add Grid containers and items and transform the Grid items to Links

--- src/components/ui/Footer.js
+++ src/components/ui/Footer.js
@@ -1,6 +1,9 @@
 import React from "react";
 
+import Grid from "@material-ui/core/Grid";
+
 import { makeStyles } from "@material-ui/styles";
+import { Link } from "react-router-dom";
 
 import footerAdornment from "../../assets/Footer Adornment.svg";
 
@@ -21,6 +24,19 @@ const useStyles = makeStyles((theme) => ({
       width: "15em",
     },
   },
+  mainContainer: {
+    position: "absolute",
+  },
+  link: {
+    color: "white",
+    fontFamily: "Arial",
+    fontSize: "0.75rem",
+    fontWeight: "bold",
+    textDecoration: "none",
+  },
+  gridItem: {
+    margin: "3em",
+  },
 }));
 
 function Footer() {
@@ -28,6 +44,105 @@ function Footer() {
 
   return (
     <footer className={classes.footer}>
+      <Grid container className={classes.mainContainer} justifyContent="center">
+        <Grid item className={classes.gridItem}>
+          <Grid container direction="column" spacing={2}>
+            <Grid item className={classes.link} component={Link} to="/">
+              Home
+            </Grid>
+          </Grid>
+        </Grid>
+        <Grid item className={classes.gridItem}>
+          <Grid container direction="column" spacing={2}>
+            <Grid item className={classes.link} component={Link} to="/services">
+              Services
+            </Grid>
+            <Grid
+              item
+              className={classes.link}
+              component={Link}
+              to="/services/software"
+            >
+              Software Development
+            </Grid>
+            <Grid
+              item
+              className={classes.link}
+              component={Link}
+              to="/services/mobile"
+            >
+              Mobile App Development
+            </Grid>
+            <Grid
+              item
+              className={classes.link}
+              component={Link}
+              to="/services/website"
+            >
+              Website Development
+            </Grid>
+          </Grid>
+        </Grid>
+        <Grid item className={classes.gridItem}>
+          <Grid container direction="column" spacing={2}>
+            <Grid
+              item
+              className={classes.link}
+              component={Link}
+              to="/revolution"
+            >
+              The Revolution
+            </Grid>
+            <Grid
+              item
+              className={classes.link}
+              component={Link}
+              to="/revolution"
+            >
+              Mission
+            </Grid>
+            <Grid
+              item
+              className={classes.link}
+              component={Link}
+              to="/revolution"
+            >
+              Technology
+            </Grid>
+            <Grid
+              item
+              className={classes.link}
+              component={Link}
+              to="/revolution"
+            >
+              Process
+            </Grid>
+          </Grid>
+        </Grid>
+        <Grid item className={classes.gridItem}>
+          <Grid container direction="column" spacing={2}>
+            <Grid item className={classes.link} component={Link} to="/about">
+              About Us
+            </Grid>
+            <Grid item className={classes.link} component={Link} to="/about">
+              Vision
+            </Grid>
+            <Grid item className={classes.link} component={Link} to="/about">
+              History
+            </Grid>
+            <Grid item className={classes.link} component={Link} to="/about">
+              Team
+            </Grid>
+          </Grid>
+        </Grid>
+        <Grid item className={classes.gridItem}>
+          <Grid container direction="column" spacing={2}>
+            <Grid item className={classes.link} component={Link} to="/contact">
+              Contact Us
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
       <img
         className={classes.adornment}
         src={footerAdornment}
