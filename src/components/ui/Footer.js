14a185d3847e1c7813d7c8ba431fcd6083133c83
Ahmet Ustun <austu@softserveinc.com>
2022-09-06 19:42:59 +0300

    Add the social network logos and set them as anchor tags

--- src/components/ui/Footer.js
+++ src/components/ui/Footer.js
@@ -7,6 +7,9 @@ import { makeStyles } from "@material-ui/styles";
 import { Link } from "react-router-dom";
 
 import footerAdornment from "../../assets/Footer Adornment.svg";
+import facebookLogo from "../../assets/facebook.svg";
+import twitterLogo from "../../assets/twitter.svg";
+import instagramLogo from "../../assets/instagram.svg";
 
 const useStyles = makeStyles((theme) => ({
   footer: {
@@ -37,6 +40,22 @@ const useStyles = makeStyles((theme) => ({
   },
   gridItem: {
     margin: "3em",
+  },
+  logo: {
+    height: "4em",
+    width: "4em",
+    [theme.breakpoints.down("xs")]: {
+      height: "2.5em",
+      width: "2.5em",
+    },
+  },
+  socialContainer: {
+    position: "absolute",
+    marginTop: "-6em",
+    right: "1.5em",
+    [theme.breakpoints.down("xs")]: {
+      right: "0.6em",
+    },
   },
 }));
 
@@ -218,6 +237,48 @@ function Footer(props) {
         src={footerAdornment}
         alt="Decorative Black Slash"
       />
+      <Grid
+        container
+        className={classes.socialContainer}
+        justifyContent="flex-end"
+        spacing={2}
+      >
+        <Grid
+          item
+          component={"a"}
+          href="https://www.facebook.com/"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          <img
+            className={classes.logo}
+            src={facebookLogo}
+            alt="Facebook Logo"
+          />
+        </Grid>
+        <Grid
+          item
+          component={"a"}
+          href="https://twitter.com/"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          <img className={classes.logo} src={twitterLogo} alt="Twitter Logo" />
+        </Grid>
+        <Grid
+          item
+          component={"a"}
+          href="https://www.instagram.com/"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          <img
+            className={classes.logo}
+            src={instagramLogo}
+            alt="Instagram Logo"
+          />
+        </Grid>
+      </Grid>
     </footer>
   );
 }
