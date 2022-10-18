f6c0dbb50f4fd8e6e4e9551d023723957cb6037b
Ahmet Ustun <austu@softserveinc.com>
2022-10-18 09:35:39 +0300

    Create the Grid structure and custom-style it

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,14 +1,72 @@
 import React from "react";
 
-// import { makeStyles } from "@material-ui/styles";
+import { makeStyles, useTheme } from "@material-ui/styles";
+import Grid from "@material-ui/core/Grid";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+
 import Lottie from "react-lottie";
 
 import animationData from "../animations/landingAnimation/data.js";
+import ButtonArrow from "./ui/ButtonArrow.js";
 
-// const useStyles = makeStyles((theme) => ({}));
+const useStyles = makeStyles((theme) => ({
+  animation: {
+    minWidth: "21em",
+    maxWidth: "50em",
+    marginTop: "2em",
+    marginLeft: "10%",
+    [theme.breakpoints.down("sm")]: {
+      maxWidth: "30em",
+    },
+  },
+  estimateButton: {
+    ...theme.typography.estimate,
+    backgroundColor: theme.palette.common.orange,
+    borderRadius: 50,
+    height: 45,
+    width: 145,
+    marginRight: 40,
+    "&:hover": {
+      backgroundColor: theme.palette.secondary.light,
+    },
+  },
+  buttonContainer: {
+    marginTop: "1em",
+  },
+  learnButtonHero: {
+    borderColor: theme.palette.common.blue,
+    borderWidth: 2,
+    borderRadius: 50,
+    textTransform: "none",
+    color: theme.palette.common.blue,
+    fontFamily: "Roboto",
+    fontWeight: "bold",
+    fontSize: "0.9rem",
+    height: 45,
+    width: 145,
+  },
+  mainContainer: {
+    marginTop: "5em",
+    [theme.breakpoints.down("md")]: {
+      marginTop: "3em",
+    },
+    [theme.breakpoints.down("xs")]: {
+      marginTop: "2em",
+    },
+  },
+  heroTextContainer: {
+    minWidth: "21.5em",
+    marginLeft: "1em",
+    [theme.breakpoints.down("xs")]: {
+      marginLeft: 0,
+    },
+  },
+}));
 
 function LandingPage() {
-  // const classes = useStyles();
+  const classes = useStyles();
+  const theme = useTheme();
 
   const defaultOptions = {
     loop: true,
@@ -19,7 +77,45 @@ function LandingPage() {
     },
   };
 
-  return <Lottie options={defaultOptions} height={"100%"} width={"100%"} />;
+  return (
+    <Grid container className={classes.mainContainer} direction="column">
+      <Grid item>
+        <Grid container justifyContent="flex-end" alignItems="center">
+          <Grid sm item className={classes.heroTextContainer}>
+            <Typography variant="h2" align="center">
+              Bringing West Coast Technology
+              <br />
+              to the Midwest
+            </Typography>
+            <Grid
+              container
+              className={classes.buttonContainer}
+              justifyContent="center"
+            >
+              <Grid item>
+                <Button className={classes.estimateButton} variant="contained">
+                  Free Estimate
+                </Button>
+              </Grid>
+              <Grid item>
+                <Button className={classes.learnButtonHero} variant="outlined">
+                  <span style={{ marginRight: 10 }}>Learn More</span>
+                  <ButtonArrow
+                    width={15}
+                    height={15}
+                    fill={theme.palette.common.blue}
+                  />
+                </Button>
+              </Grid>
+            </Grid>
+          </Grid>
+          <Grid sm item className={classes.animation}>
+            <Lottie options={defaultOptions} height={"100%"} width={"100%"} />
+          </Grid>
+        </Grid>
+      </Grid>
+    </Grid>
+  );
 }
 
 export default LandingPage;
