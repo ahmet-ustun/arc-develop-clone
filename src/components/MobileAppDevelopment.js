9e4f0584beb03045a9f0a7d338b5ca1625e6b07a
Ahmet Ustun <austu@softserveinc.com>
2022-10-31 10:30:58 +0200

    Create the MobileAppDevelopment component and style inside

---
+++ src/components/MobileAppDevelopment.js
@@ -0,0 +1,264 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+import { makeStyles, useTheme } from "@material-ui/styles";
+import useMediaQuery from "@material-ui/core/useMediaQuery";
+
+import Grid from "@material-ui/core/Grid";
+import IconButton from "@material-ui/core/IconButton";
+import Typography from "@material-ui/core/Typography";
+import Hidden from "@material-ui/core/Hidden";
+
+import Lottie from "react-lottie";
+
+import integrationAnimation from "../animations/integrationAnimation/data.json";
+
+import prevArrow from "../assets/backArrow.svg";
+import nextArrow from "../assets/forwardArrow.svg";
+import swissKnife from "../assets/swissKnife.svg";
+import accessPaper from "../assets/extendAccess.svg";
+import engagement from "../assets/increaseEngagement.svg";
+
+import CallToAction from "./ui/CallToAction.js";
+
+const useStyles = makeStyles((theme) => ({
+  rowContainer: {
+    paddingLeft: "5em",
+    paddingRight: "5em",
+    [theme.breakpoints.down("sm")]: {
+      paddingLeft: "3em",
+      paddingRight: "3em",
+    },
+  },
+  heading: {
+    maxWidth: "40em",
+  },
+  arrowContainer: {
+    marginTop: "0.5em",
+  },
+}));
+
+function MobileAppDevelopment({ setTabValue, setItemIndex }) {
+  const classes = useStyles();
+  const theme = useTheme();
+
+  const matchesMD = useMediaQuery(theme.breakpoints.down("md"));
+  const matchesSM = useMediaQuery(theme.breakpoints.down("sm"));
+  const matchesXS = useMediaQuery(theme.breakpoints.down("xs"));
+
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: integrationAnimation,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice",
+    },
+  };
+
+  return (
+    <Grid container direction="column">
+      <Grid
+        item
+        container
+        className={classes.rowContainer}
+        justifyContent={matchesMD && "center"}
+        style={{ marginTop: "2em" }}
+      >
+        <Hidden mdDown>
+          <Grid
+            item
+            className={classes.arrowContainer}
+            style={{ marginLeft: "-3.5em", marginRight: "1em" }}
+          >
+            <IconButton
+              style={{ background: "transparent" }}
+              component={Link}
+              to="/services/software"
+              onClick={() => {
+                setTabValue(1);
+                setItemIndex(1);
+              }}
+            >
+              <img src={prevArrow} alt="Back to Software Development" />
+            </IconButton>
+          </Grid>
+        </Hidden>
+        <Grid item container className={classes.heading} direction="column">
+          <Grid item>
+            <Typography
+              variant="h2"
+              align={matchesMD ? "center" : "left"}
+              gutterBottom
+            >
+              Mobile App Development
+            </Typography>
+          </Grid>
+          <Grid item>
+            <Typography
+              variant="body1"
+              align={matchesMD ? "center" : "left"}
+              paragraph
+            >
+              Mobile apps allow you to take your tools on the go.
+            </Typography>
+            <Typography
+              variant="body1"
+              align={matchesMD ? "center" : "left"}
+              paragraph
+            >
+              Whether you want an app for your customers, employees, or
+              yourself, we can build cross-platform native solutions for any
+              part of your business process. This opens you up to a whole new
+              world of possibilities by taking advantage of phone features like
+              the camera, GPS, push notifications, and more.
+            </Typography>
+            <Typography
+              variant="body1"
+              align={matchesMD ? "center" : "left"}
+              paragraph
+            >
+              Convenience. Connection.
+            </Typography>
+          </Grid>
+        </Grid>
+        <Hidden mdDown>
+          <Grid item className={classes.arrowContainer}>
+            <IconButton
+              style={{ background: "transparent" }}
+              component={Link}
+              to="/services/website"
+              onClick={() => {
+                setTabValue(1);
+                setItemIndex(3);
+              }}
+            >
+              <img src={nextArrow} alt="Forward to Website Development" />
+            </IconButton>
+          </Grid>
+        </Hidden>
+      </Grid>
+      <Grid
+        item
+        container
+        className={classes.rowContainer}
+        direction={matchesSM ? "column" : "row"}
+        style={{ margin: "15em auto" }}
+      >
+        <Grid item container direction="column" md>
+          <Grid item>
+            <Typography
+              variant="h4"
+              align={matchesSM ? "center" : "left"}
+              gutterBottom
+            >
+              Integration
+            </Typography>
+          </Grid>
+          <Grid item>
+            <Typography
+              variant="body1"
+              align={matchesSM ? "center" : "left"}
+              paragraph
+            >
+              Our technology enables an innate interconnection between web and
+              mobile applications, putting everything you need right in one
+              convenient place.
+            </Typography>
+            <Typography
+              variant="body1"
+              align={matchesSM ? "center" : "left"}
+              paragraph
+            >
+              This allows you to extend your reach, reinvent interactions, and
+              develop a stronger relationship with your users than ever before.
+            </Typography>
+          </Grid>
+        </Grid>
+        <Grid item md>
+          <Lottie options={defaultOptions} style={{ maxWidth: "20em" }} />
+        </Grid>
+        <Grid item container direction="column" md>
+          <Grid item>
+            <Typography
+              variant="h4"
+              align={matchesSM ? "center" : "left"}
+              gutterBottom
+            >
+              Platform Support
+            </Typography>
+          </Grid>
+          <Grid item>
+            <Typography
+              variant="body1"
+              align={matchesSM ? "center" : "left"}
+              paragraph
+            >
+              Our cutting-edge development process allows us to create apps for
+              iPhone, Android, and tablets — all at the same time.
+            </Typography>
+            <Typography
+              variant="body1"
+              align={matchesSM ? "center" : "left"}
+              paragraph
+            >
+              This significantly reduces costs and creates a more unified brand
+              experience across all devices.
+            </Typography>
+          </Grid>
+        </Grid>
+      </Grid>
+      <Grid
+        item
+        container
+        className={classes.rowContainer}
+        direction={matchesMD ? "column" : "row"}
+        style={{ marginBottom: "15em" }}
+      >
+        <Grid item container direction="column" alignItems="center" md>
+          <Grid item>
+            <Typography variant="h4" align="center" gutterBottom>
+              Extend Service
+            </Typography>
+          </Grid>
+          <Grid item>
+            <img src={swissKnife} alt="Swiss Army Knife" />
+          </Grid>
+        </Grid>
+        <Grid
+          item
+          container
+          direction="column"
+          alignItems="center"
+          style={{ margin: matchesMD && "10em auto" }}
+          md
+        >
+          <Grid item>
+            <Typography variant="h4" align="center" gutterBottom>
+              Extend Access
+            </Typography>
+          </Grid>
+          <Grid item>
+            <img
+              src={accessPaper}
+              alt="Tear-One-Off Sign"
+              style={{ maxWidth: matchesXS ? "20em" : "28em" }}
+            />
+          </Grid>
+        </Grid>
+        <Grid item container direction="column" alignItems="center" md>
+          <Grid item>
+            <Typography variant="h4" align="center" gutterBottom>
+              Engage Users
+            </Typography>
+          </Grid>
+          <Grid item>
+            <img src={engagement} alt="App Notification" />
+          </Grid>
+        </Grid>
+      </Grid>
+      <CallToAction setTabValue={setTabValue} />
+    </Grid>
+  );
+}
+
+export default MobileAppDevelopment;
