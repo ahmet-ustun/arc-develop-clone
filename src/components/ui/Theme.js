86fe5aef6ee8ec09ec14f4645f4cd112dd02aa92
Ahmet Ustun <austu@softserveinc.com>
2022-11-08 10:50:34 +0200

    Add some custom styling to override the Input-related props

--- src/components/ui/Theme.js
+++ src/components/ui/Theme.js
@@ -78,4 +78,26 @@ export default createTheme({
       color: arcGray,
     },
   },
+  overrides: {
+    MuiInputLabel: {
+      root: {
+        color: arcBlue,
+        fontSize: "1rem",
+      },
+    },
+    MuiInput: {
+      root: {
+        color: arcGray,
+        fontWeight: 300,
+      },
+      underline: {
+        "&:before": {
+          borderBottom: `2px solid ${arcBlue}`,
+        },
+        "&:hover:not($disabled):not($focused):not($error):before": {
+          borderBottom: `2px solid ${arcBlue}`,
+        },
+      },
+    },
+  },
 });
